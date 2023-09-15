3abe7568d969f6e9f811c52417bb8dea10fb9d7e
Ariel Leyva <aleyva1509@gmail.com>
2023-09-14 21:27:39 -0600

    feat: first raylib example

--- src/main.c
+++ src/main.c
@@ -1,7 +1,23 @@
-#include <stdlib.h>
-#include <stdio.h>
+#include "raylib.h"
 
-int main(int argc, char *argv[]) {
-    printf("Hello World!\n");
-    return EXIT_SUCCESS;
+int main(void)
+{
+    // create a window
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+    // wait 3 seconds
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+    CloseWindow();
+
+    // exit
+    return 0;
 }
