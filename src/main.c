0155be654c4cbb61ee7392425720167aae09c663
Javier Vasquez <javiervasquez212@gmail.com>
2023-09-15 18:25:47 -0600

    feat: Added platform + fixed Images

--- src/main.c
+++ src/main.c
@@ -1,15 +1,14 @@
 #include "raylib.h"
 
-// Definición de constantes para evitar magicnumbers
+// Definición de constantes para evitar magic numbers
 const int screenWidth = 700;
 const int screenHeight = 598;
 const float playerSpeed = 4.0f;
-const float jumpForce = 10.0f;
+const float jumpForce = 15.0f;
 const float gravity = 0.5f;
 
 int main(void)
 {
-
     // Inicialización de la ventana y el audio
     InitWindow(screenWidth, screenHeight, "El fruto del árbol envenenado");
     InitAudioDevice();
@@ -17,12 +16,13 @@ int main(void)
     // Se cargan las texturas y el audio
     Texture2D playerTex = LoadTexture("../../FAE/src/img/playerTexture.png");
     Texture2D backgroundTex = LoadTexture("../../FAE/src/img/background.png");
+    Texture2D platformTex = LoadTexture("../../FAE/src/img/Platform.png");
     Sound backgroundSoundtrack = LoadSound("../../FAE/src/soundtracks/backgroundSountrack.wav");
 
     // Constante de la altura del piso, para validar movimiento
     const int floorHeight = screenHeight - playerTex.height;
 
-    // Definimos los vectores, uno para la posición y otro para la velocidad(salto)
+    // Definimos los vectores, uno para la posición y otro para la velocidad (salto)
     Vector2 playerPosition = {playerTex.width, floorHeight};
     Vector2 playerVelocity = {0.0f, 0.0f}; // La velocidad comienza en 0
 
@@ -36,6 +36,11 @@ int main(void)
 
     while (!WindowShouldClose())
     {
+
+        // Calcula los límites del jugador y la plataforma
+        Rectangle playerBounds = {playerPosition.x, playerPosition.y, playerTex.width, playerTex.height};
+        Rectangle platformBounds = {200, 400, platformTex.width, platformTex.height};
+
         if (IsKeyDown(KEY_RIGHT) && playerPosition.x + playerTex.width < screenWidth)
             playerPosition.x += playerSpeed;
         if (IsKeyDown(KEY_LEFT) && playerPosition.x > 0)
@@ -44,7 +49,7 @@ int main(void)
         // Validar si puede saltar
         if (IsKeyDown(KEY_UP) && !isJumping)
         {
-            // Validar que caiga en el piso y no se hunda
+
             if (playerPosition.y >= floorHeight)
             {
                 playerVelocity.y = -jumpForce;
@@ -52,8 +57,10 @@ int main(void)
             }
         }
 
-        // Ubicación segun velocidad y posición
+        // Aplica gravedad
         playerVelocity.y += gravity;
+
+        // Actualiza la posición según la velocidad
         playerPosition.x += playerVelocity.x;
         playerPosition.y += playerVelocity.y;
 
@@ -69,6 +76,7 @@ int main(void)
         ClearBackground(RAYWHITE);
         DrawTexture(backgroundTex, 0, 0, WHITE);
         DrawTextureV(playerTex, playerPosition, WHITE);
+        DrawTexture(platformTex, 200, 400, WHITE);
         EndDrawing();
     }
 
